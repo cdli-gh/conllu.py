19bfa9ef9e8fc3f20c7cb92b4a30e04d94e9a89c
Émilie Pagé-Perron <epp@irkalla.net>
2017-07-26 20:32:11 -0400

    Update convert.py

--- convert.py
+++ convert.py
@@ -6,7 +6,8 @@ import os
 import sys
 import codecs
 
-from conllu import conllu
+# from conllu import *
+import conllu
 
 def argparser():
     import argparse
@@ -22,13 +23,13 @@ def output_document_text(document, output, options=None):
 def output_document_annotations(document, output, options=None):
     for annotation in document.to_brat_standoff():
         print >> output, unicode(annotation)
-    
+
 def output_document(document, options=None):
     """Output given document according to given options."""
     if options is None or options.output is None:
         # If no output directory is specified, output both to stdout
-        output_document_text(document, sys.stdout, options)
-        output_document_annotations(document, sys.stdout, options)
+        output_document_text(document, output_text, options)
+        output_document_annotations(document, output_file, options)
     else:
         basefn = os.path.splitext(os.path.basename(document.filename))[0]
         txtfn = os.path.join(options.output, basefn+'.txt')
@@ -42,7 +43,7 @@ def convert(source, options=None):
     # TODO: support conversions other than CoNLL-U to brat.
     for document in conllu.read_documents(source):
         output_document(document, options)
-    
+
 def main(argv):
     args = argparser().parse_args(argv[1:])
     for fn in args.file:
@@ -50,4 +51,13 @@ def main(argv):
     return 0
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+   # sys.exit(main(sys.argv))
+   # my_file = open("train-sample.conllu")
+   # my_file_lines = my_file.readline()
+   # my_file_list = []
+   # my_file_list.append(my_file_lines)
+   output_file = open("output.ann", "wt")
+   output_text = open("output.txt", "wt")
+   convert(open((raw_input("file path: "))))
+   # output_file.close()
+   # print("hi")
